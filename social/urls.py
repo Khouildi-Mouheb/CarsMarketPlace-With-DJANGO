ee716dffbfa6185bd5bd0744a409248a1b4b024a
Mouheb <mouheb.khouildi@etudiant-enit.utm.tn>
2025-04-11 20:05:06 +0100

    i added the comment class with his crud operations

--- social/urls.py
+++ social/urls.py
@@ -6,5 +6,8 @@ urlpatterns=[
     path('create_post/<int:pk>/',views.create_post_view,name='create_post'),
     path('delete_post/<int:pk>/',views.delete_post_view,name='delete_post'),
     path('edit_post/<int:pk>/',views.edit_post_view,name='edit_post'),
+    path("comments/create_comment/<int:post_id>/",views.create_comment_view,name='create_comment'),
+    path("comments/<int:comment_id>/edit_comment/",views.edit_comment_view,name='edit_comment'),
+    path("comments/<int:comment_id>//delete_comment/",views.delete_comment_view,name='delete_comment'),
 
 ]
--- social/views.py
+++ social/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render , redirect,get_object_or_404
-from django.http import HttpResponse
-from .models import Post
+from django.http import HttpResponse , HttpResponseForbidden
+from .models import Post , Comment
 from django.contrib.auth.decorators import login_required
 from rooms.models import Room
+from django.db.models import Q
 
 
 @login_required
@@ -42,3 +43,54 @@ def edit_post_view(request,pk):
         post.save()
         return redirect('room_detail',pk=post.room.id)
     return render(request,'social/edit_post.html',{'post':post})
+
+########################  comment-views   ############################
+
+@login_required
+def create_comment_view(request,post_id):
+    post=get_object_or_404(Post,id=post_id)
+    room=post.room
+    creator_queryset=[room.creator]
+    participants_queryset=list(room.participants.all())
+    members=creator_queryset+participants_queryset
+    ids=[participant.id for participant in room.participants.all()]
+    ids.append(room.creator.id)
+    if request.user.id not in ids:
+    #if request.user.id not in room.participants.values_list('id',flat=True) and request.user.id!=room.creator.id and request.user.id!=post.author.id:
+        return HttpResponse("you are not allowed to comment on this post !")    
+    else:
+        if request.method=='POST':
+            content=request.POST.get('content')
+            post.comments.create(
+                author=request.user,
+                content=content,
+            )
+            #redirect to the room
+            return redirect('room_detail',pk=post.room.id)
+        
+    return render(request,'social/create_comment.html',{'post':post})
+
+
+
+
+@login_required
+def delete_comment_view(request,comment_id):
+    comment=get_object_or_404(Comment,id=comment_id)
+    if comment.author!=request.user:
+        return HttpResponseForbidden('you are not allowed to delete this comment')
+    comment.delete()
+    return redirect('room_detail',pk=comment.post.room.id)
+
+
+
+@login_required
+def edit_comment_view(request,comment_id):
+    comment=get_object_or_404(Comment,id=comment_id)
+    if comment.author!=request.user:
+        return HttpResponseForbidden('you are not allowed to edit this comment')
+    
+    if request.method=='POST':
+        comment.content=request.POST.get('content')
+        comment.save()
+        return redirect('room_detail',pk=comment.post.room.id)
+    return render(request,'social/edit_comment.html',{'comment':comment})
