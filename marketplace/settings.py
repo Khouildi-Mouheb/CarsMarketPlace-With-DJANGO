1c36f4a56fe90670045f8e9e0ef1e1623468957b
Mouheb <mouheb.khouildi@etudiant-enit.utm.tn>
2025-04-11 20:25:10 +0100

    connection with the static folder

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Adjust the path as needed
+    ]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
